1c4b31be2179d69cf109fec99e414e399de2bf27
Daniel Jermy <70345903+djermy@users.noreply.github.com>
2023-06-20 08:53:55 +0000

    Created and tested timer

---
+++ main.py
@@ -0,0 +1,34 @@
+import os, sys
+import time as t
+
+def alert():
+    return os.system('spd-say "attention, your timer has finished"')
+
+def timer(arg):
+    minutes = arg * 60
+    os.system(f'spd-say "your timer has been set for {str(arg)} minutes"')
+    t.sleep(minutes)
+    alert()
+
+def validate_argument():
+    try:
+        arg = sys.argv[1]
+        arg = float(arg)
+        if arg == 0.0:
+            print('error! 0 is not valid')
+            return None
+        return arg
+    except:
+        print('invalid input, must be a number')
+        return None
+
+def main():
+    arg = validate_argument()
+    if arg == None:
+        print('error')
+        return 0
+    timer(arg)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
